7bfeb28da93764872099dd99c71cac1e5a9e1a95
Thomaz Falbo <thomazfalbo@outlook.com>
2022-08-15 12:13:19 -0300

    Precisa ser corrigido um bug que não mostra a primeira carta

---
+++ js/game.js
@@ -0,0 +1,33 @@
+const grid = document.querySelector('.grid')
+
+const characters = ['mage', 'soldier', 'cleric', 'thief']
+
+const createElement = (tag, className) => {
+  const element = document.createElement(tag)
+  element.className = className
+  return element
+}
+
+const createCard = character => {
+  const card = createElement('div', 'card')
+  const front = createElement('div', 'face front')
+  const back = createElement('div', 'face back')
+  front.style.backgroundImage = `url('../images/${character}.png')`
+
+  card.appendChild(front)
+  card.appendChild(back)
+
+  return card
+}
+
+const loadgame = () => {
+  const duplicateCharacters = [...characters, ...characters]
+  const shuffledArray = duplicateCharacters.sort(() => Math.random() - 0.5)
+
+  shuffledArray.forEach(character => {
+    const card = createCard(character)
+    grid.appendChild(card)
+  })
+}
+
+loadgame()
